46f38fdef235b928000f51461767c2793d0ee863
Jean SIMARD <woshilapin@tuziwo.info>
2020-04-05 17:40:21 +0200

    Remove useless module in refinery_core

--- refinery_core/src/lib.rs
+++ refinery_core/src/lib.rs
@@ -1,5 +1,3 @@
-pub mod config;
-mod drivers;
 mod error;
 mod runner;
 mod traits;
@@ -13,18 +11,3 @@ pub use crate::util::{find_migration_files, MigrationType};
 
 #[cfg(feature = "rusqlite")]
 pub use rusqlite;
-
-#[cfg(feature = "postgres")]
-pub use postgres;
-
-#[cfg(feature = "mysql")]
-pub use mysql;
-
-#[cfg(feature = "tokio-postgres")]
-pub use tokio_postgres;
-
-#[cfg(feature = "mysql_async")]
-pub use mysql_async;
-
-#[cfg(feature = "tokio")]
-pub use tokio;
