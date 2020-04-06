9b9be5c3560bd6b78e1916879db7daa843dee055
Jean SIMARD <woshilapin@tuziwo.info>
2020-04-07 00:30:48 +0200

    Inline and simplify the function migration_fn_quoted

--- refinery_macros/src/lib.rs
+++ refinery_macros/src/lib.rs
@@ -3,28 +3,11 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use proc_macro2::{Span as Span2, TokenStream as TokenStream2};
+use proc_macro2::Span as Span2;
 use quote::quote;
-use quote::ToTokens;
 use std::ffi::OsStr;
 use std::path::PathBuf;
 use syn::Ident;
-
-fn migration_fn_quoted<T: ToTokens>(_migrations: Vec<T>) -> TokenStream2 {
-    let result = quote! {
-        use refinery::Migration;
-        pub fn runner() {
-            let quoted_migrations: Vec<(&str, String)> = vec![#(#_migrations),*];
-            let mut migrations: Vec<Migration> = Vec::new();
-            for module in quoted_migrations.into_iter() {
-                migrations.push(Migration {
-                    name: "initial".into(),
-                });
-            }
-        }
-    };
-    result
-}
 
 #[proc_macro]
 pub fn include_migration_mods(_: TokenStream) -> TokenStream {
@@ -42,11 +25,16 @@ pub fn include_migration_mods(_: TokenStream) -> TokenStream {
         migrations_mods.push(mig_mod);
     }
 
-    let fnq = migration_fn_quoted(_migrations);
     let result = quote! {
         #(#migrations_mods)*
 
-        #fnq
+        use refinery::Migration;
+        pub fn runner() {
+            let quoted_migrations: Vec<(&str, String)> = vec![#(#_migrations),*];
+            let migrations = vec![Migration {
+                name: "initial".into(),
+            }];
+        }
     };
     result.into()
 }
