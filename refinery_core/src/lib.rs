0ac3046f19f735bd43b6e89c0a99a3cdc5613d4e
Jean SIMARD <woshilapin@tuziwo.info>
2020-04-07 00:02:05 +0200

    Remove useless field sql in Migration

--- refinery_core/src/lib.rs
+++ refinery_core/src/lib.rs
@@ -3,11 +3,10 @@ use regex::Regex;
 #[derive(Clone, Debug)]
 pub struct Migration {
     pub name: String,
-    pub sql: String,
 }
 
 impl Migration {
-    pub fn from_filename(name: &str, sql: &str) -> Result<Migration, String> {
+    pub fn from_filename(name: &str) -> Result<Migration, String> {
         let captures = Regex::new(r"^([^m]\w+)")
             .unwrap()
             .captures(name)
@@ -16,9 +15,6 @@ impl Migration {
 
         let name = (&captures[1]).into();
 
-        Ok(Migration {
-            name,
-            sql: sql.into(),
-        })
+        Ok(Migration { name })
     }
 }
